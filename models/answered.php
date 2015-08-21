c5b41bac80840ea1f653515890f024698f597895
dolkalaima <dolka.laima@gmail.com>
2015-08-21 15:57:06 +0300

    jstest

--- models/answered.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('/openserver/OpenServer/domains/project.my/models/reporter.php');
-include_once('/openserver/OpenServer/domains/project.my/models/admin.php');
-db_connect('127.0.0.1','questions','root','');
-
-$current_q = get_question_for_reporter();
-$CURRENT_Q_ID = $current_q['id'];
-answer_question($CURRENT_Q_ID);
-header('Location: http://project.my/templates/view/admin/index.php');
--- templates/view/ask/ask_confirm.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-
-<script>
-        alert("Your question has been successfully added!");
-
-</script>
